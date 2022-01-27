1f65028fc3cd1de4b8c62dada641549ea97477cf
Anko Anchev <anko.anchev@gmail.com>
2022-01-27 17:11:59 +0000

    added basic pattern matching for cards

--- Belote/Player/Ai/Basic/BasicAiPlayer.cs
+++ Belote/Player/Ai/Basic/BasicAiPlayer.cs
@@ -7,15 +7,30 @@ namespace Belote.Player.Ai.Basic
 {
     public class BasicAiPlayer : IPlayer
     {
+        private static readonly IReadOnlyList<IReadOnlyList<Card>> BidPatterns = new[]
+        {
+            new [] {Card.Spades_J, Card.Spades_9},
+            new [] {Card.Spades_J, Card.Diamonds_A, Card.Hearts_A}
+        };
+
+
+        // ReSharper disable once InconsistentNaming
         private IPlayerStateView? State;
-        
+
         public void BindStateView(IPlayerStateView playerStateView)
         {
             State = playerStateView;
         }
+        #pragma warning disable CS8602
 
         public Contract? Bid()
         {
+            foreach (var pattern in BidPatterns)
+            {
+                var found = State.CurrentHand.MatchRanks(pattern);
+                if (found != null)
+                    return ContractUtils.PlainContracts[found[0].Suit()];
+            }
             return null;
         }
 
@@ -23,5 +38,7 @@ namespace Belote.Player.Ai.Basic
         {
             throw new NotImplementedException();
         }
+
+        #pragma warning restore CS8602
     }
 }
---
+++ Belote/Player/Ai/GameUtils.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Belote.Domain;
+
+namespace Belote.Player.Ai
+{
+    public static class GameUtils
+    {
+        public static List<Card>? MatchRanks(this IEnumerable<Card> hand, IEnumerable<Card> target, int[]? suitMapping = null)
+        {
+            var suits = suitMapping ?? new[] {-1, -1, -1, -1};
+
+            // ReSharper disable PossibleMultipleEnumeration
+            var result = new List<Card>(target.Count());
+            foreach (var card in target)
+            {
+                var found = hand.FindCard(card, suits);
+                if (found == null) return null;
+                suits[card.Suit()] = found.Value.Suit();
+                result.Add(found.Value);
+            }
+
+            return result;
+        }
+
+        public static Card? FindCard(this IEnumerable<Card> hand, Card target, int[] suitRemapping)
+        {
+            var targetRank = target.Rank();
+            var targetSuit = suitRemapping[target.Suit()];
+            return hand.FirstOrDefault(c => c.Rank() == targetRank && (targetSuit < 0 || c.Suit() == targetSuit) );
+        }
+    }
+}
