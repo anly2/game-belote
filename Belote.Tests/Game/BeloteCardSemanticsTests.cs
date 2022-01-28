9c6659b5a576dce6ca684869f3aac12e4f1fa93a
Anko Anchev <anko.anchev@gmail.com>
2022-01-28 17:40:54 +0000

    Added SortHand test

---
+++ Belote.Tests/Game/BeloteCardSemanticsTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using Belote.Domain;
+using Belote.Game;
+using Xunit;
+using static AssertNet.Assertions;
+
+namespace Belote.Tests.Game;
+
+public class CardSemanticsTests
+{
+    private static List<Card> GetTestCardHand()
+    {
+        return new List<Card>(new []
+        {
+            Card.Diamonds_8, Card.Diamonds_7, Card.Diamonds_9,
+            Card.Spades_A, Card.Spades_J, Card.Spades_10,
+            Card.Clubs_K,
+            Card.Hearts_Q
+        });
+    }
+
+
+    [Fact]
+    public void CardHands_CanBeSorted_ByPower_AscSuitDescRank_NoContract()
+    {
+        var hand = GetTestCardHand();
+
+        hand.SortHand();
+
+        AssertThat(hand).ContainsExactly(
+            Card.Clubs_K,
+            Card.Diamonds_9, Card.Diamonds_8, Card.Diamonds_7,
+            Card.Hearts_Q,
+            Card.Spades_A, Card.Spades_10, Card.Spades_J
+        );
+    }
+
+    [Fact]
+    public void CardHands_CanBeSorted_ByPower_AscSuitDescRank_OneTrumpContract()
+    {
+        var hand = GetTestCardHand();
+
+        hand.SortHand(Contract.Spades);
+
+        AssertThat(hand).ContainsExactly(
+            Card.Clubs_K,
+            Card.Diamonds_9, Card.Diamonds_8, Card.Diamonds_7,
+            Card.Hearts_Q,
+            Card.Spades_J, Card.Spades_A, Card.Spades_10
+        );
+    }
+}
