f44d3b6786c0123cc876944d781ff0b4631232be
Anko Anchev <anko.anchev@gmail.com>
2022-01-29 22:48:26 +0000

    Added a test for game score rounding

---
+++ Belote.Tests/Game/GameTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Belote.Domain;
+using Belote.Game;
+using Xunit;
+using static AssertNet.Assertions;
+
+namespace Belote.Tests.Game;
+
+public class GameTests
+{
+    [Fact]
+    public void ScoringPile_onPlainSuitContract_RoundsDownForWinner()
+    {
+        var deck = Belote.Game.Game.GetPlayingDeck();
+
+        deck.Remove(Card.Diamonds_9);
+        deck.Remove(Card.Spades_Q);
+        deck.Remove(Card.Diamonds_J);
+        deck.Remove(Card.Diamonds_10);
+
+        var contract = Contract.Hearts;
+
+
+        //FIXME: test Game.CountScore
+        //FIXME: test Game.MatchScoreToGameScore
+        var score = deck.Sum(c => c.Value(contract));
+
+        AssertThat(score).IsEqualTo(156);
+        //TODO: game score should be 15 , but naive rounding gives 16
+    }
+}
