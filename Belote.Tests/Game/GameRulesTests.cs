158341f11b895885338d15d41324c4f6c24b7413
Anko Anchev <anko.anchev@gmail.com>
2022-01-30 19:43:43 +0000

    reworked the card.CanBePlayed into hand.PlayableCards

--- Belote.Tests/Game/GameRulesTests.cs
+++ Belote.Tests/Game/GameRulesTests.cs
@@ -9,7 +9,55 @@ namespace Belote.Tests.Game;
 public class GameRulesTests
 {
     [Fact]
-    public void CanBePlayed_ForcesTrumping_WhenAskedNotAvailableButATrumpCardIs()
+    public void PlayableCards_AllowsAnything_WhenNothingAskedYet()
+    {
+        //Contract: AllTrumps
+        //Trick:    -
+        //Hand:     7♣, Q♣, 10♣, 7♥, K♥, 10♥, J♥, 9♠
+        //expected: ✓ , ✓ ,  ✓  , ✓ , ✓ ,  ✓ , ✓ , ✓
+
+        var contract = Contract.AllTrumps;
+        var trick = System.Array.Empty<Card>();
+        var hand = new[] {Spades_7, Spades_Q, Spades_10, Hearts_7, Hearts_K, Hearts_10, Hearts_J, Clubs_9};
+
+        AssertThat(hand.PlayableCards(contract, trick))
+            .ContainsExactly(Spades_7, Spades_Q, Spades_10, Hearts_7, Hearts_K, Hearts_10, Hearts_J, Clubs_9);
+    }
+
+    [Fact]
+    public void PlayableCards_ForcesSameSuit_WhenAskedIsAvailable_AnyIfNotTrumpSuit()
+    {
+        //Contract: Hearts
+        //Trick:    8♦
+        //Hand:     7♣, Q♣, 10♣, 7♦, K♥, 10♥, J♥, 9♠
+        //expected: x , x ,  x  , ✓ , x ,  x , x , x
+
+        var contract = Contract.Hearts;
+        var trick = new[] {Diamonds_8};
+        var hand = new[] {Spades_7, Spades_Q, Spades_10, Diamonds_7, Hearts_K, Hearts_10, Hearts_J, Clubs_9};
+
+        AssertThat(hand.PlayableCards(contract, trick))
+            .ContainsExactly(Diamonds_7);
+    }
+
+    [Fact]
+    public void PlayableCards_ForcesSameSuit_WhenAskedIsAvailable_StrongerIfTrumpSuit()
+    {
+        //Contract: AllTrumps
+        //Trick:    8♦
+        //Hand:     7♣, Q♣, 10♣, 7♦, K♦, 10♥, J♥, 9♠
+        //expected: x , x ,  x  , x , ✓ ,  x , x , x
+
+        var contract = Contract.AllTrumps;
+        var trick = new[] {Diamonds_8};
+        var hand = new[] {Spades_7, Spades_Q, Spades_10, Diamonds_7, Diamonds_K, Hearts_10, Hearts_J, Clubs_9};
+
+        AssertThat(hand.PlayableCards(contract, trick))
+            .ContainsExactly(Diamonds_K);
+    }
+
+    [Fact]
+    public void PlayableCards_ForcesTrumping_WhenAskedNotAvailableButATrumpCardIs_AndCurrentWinnerIsAnOpponent()
     {
         //Contract: Hearts
         //Trick:    8♦
@@ -19,10 +67,40 @@ public class GameRulesTests
         var contract = Contract.Hearts;
         var trick = new[] {Diamonds_8};
         var hand = new[] {Spades_7, Spades_Q, Spades_10, Hearts_7, Hearts_K, Hearts_10, Hearts_J, Clubs_9};
-        var expected = new[] {false, false, false, true, true, true, true, false};
 
-        AssertThat(hand).Select(c => c.CanBePlayed(contract, hand, trick))
-            .ContainsExactly(expected);
+        AssertThat(hand.PlayableCards(contract, trick))
+            .ContainsExactly(Hearts_7, Hearts_K, Hearts_10, Hearts_J);
+    }
 
+    [Fact]
+    public void PlayableCards_AllowsAnything_WhenAskedNotAvailableButATrumpCardIs_AndCurrentWinnerIsTeammate()
+    {
+        //Contract: Hearts
+        //Trick:    8♦, 7♦
+        //Hand:     7♣, Q♣, 10♣, 7♥, K♥, 10♥, J♥, 9♠
+        //expected: ✓ , ✓ ,  ✓  , ✓ , ✓ ,  ✓ , ✓ , ✓
+
+        var contract = Contract.Hearts;
+        var trick = new[] {Diamonds_8, Diamonds_7};
+        var hand = new[] {Spades_7, Spades_Q, Spades_10, Hearts_7, Hearts_K, Hearts_10, Hearts_J, Clubs_9};
+
+        AssertThat(hand.PlayableCards(contract, trick))
+            .ContainsExactly(Spades_7, Spades_Q, Spades_10, Hearts_7, Hearts_K, Hearts_10, Hearts_J, Clubs_9);
+    }
+
+    [Fact]
+    public void PlayableCards_AllowsAnything_WhenCannotRaise()
+    {
+        //Contract: AllTrumps
+        //Trick:    8♦
+        //Hand:     7♣, Q♣, 10♣, 7♥, K♥, 10♥, J♥, 9♠
+        //expected: ✓ , ✓ ,  ✓  , ✓ , ✓ ,  ✓ , ✓ , ✓
+
+        var contract = Contract.AllTrumps;
+        var trick = new[] {Diamonds_8};
+        var hand = new[] {Spades_7, Spades_Q, Spades_10, Hearts_7, Hearts_K, Hearts_10, Hearts_J, Clubs_9};
+
+        AssertThat(hand.PlayableCards(contract, trick))
+            .ContainsExactly(Spades_7, Spades_Q, Spades_10, Hearts_7, Hearts_K, Hearts_10, Hearts_J, Clubs_9);
     }
 }
--- Belote/Game/GameRules.cs
+++ Belote/Game/GameRules.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using Belote.Domain;
 
@@ -8,33 +9,45 @@ namespace Belote.Game
     {
         public static bool CanBePlayed(this Card card, Contract contract, IEnumerable<Card> handCards, IEnumerable<Card> trickCards)
         {
+            return handCards.PlayableCards(contract, trickCards).Contains(card);
+        }
+
+        public static IEnumerable<Card> PlayableCards(this IEnumerable<Card> handCards, Contract contract, IEnumerable<Card> trickCards)
+        {
             // ReSharper disable PossibleMultipleEnumeration
-            if (!trickCards.Any()) return true;
+            if (!trickCards.Any()) return handCards;
 
             var askedCard = trickCards.First();
+
+            // If the asked suit can be answered, a card of that suit must be played
             var askedSuit = askedCard.Suit();
             var askedIsNotTrump = !askedCard.IsTrump(contract);
-
-            if (askedSuit != card.Suit())
+            var sameSuitCards = handCards.Where(c => askedSuit == c.Suit());
+            if (sameSuitCards.Any())
             {
-                // has a card of the asked suit
-                if (handCards.Any(c => askedSuit == c.Suit()))
-                    return false;
+                if (askedIsNotTrump) return sameSuitCards;
 
-                // can overrule with trump
-                if (askedIsNotTrump && handCards.Any(c => c.IsTrump(contract)))
-                    return card.IsTrump(contract);
-
-                return true;
+                // If playing a trump suit, a stronger card must be played, if available
+                var strongest = trickCards.StrongestCard(contract);
+                return sameSuitCards.Where(c => strongest.CompareTo(c, contract) < 0);
             }
 
-            // Same suit
-            if (askedIsNotTrump) return true;
+            // If the asked suit cannot be answered, and the current winner is not a teammate, a trump must be played, if available
+            if (askedIsNotTrump)
+            {
+                var trumpCards = handCards.Where(c => c.IsTrump(contract));
+                if (trumpCards.Any() && !StrongestIsTeammate(trickCards, contract))
+                    return trumpCards;
+            }
 
-            // (card > strongest) OR no stronger card in hand
-            var strongest = trickCards.StrongestCard(contract);
-            return (card.CompareTo(strongest, contract) > 0)
-                   || handCards.All(c => c == card || strongest.CompareTo(c, contract) > 0);
+            // If cannot answer asked and cannot trump, can play anything but would lose it
+            return handCards;
+        }
+
+        public static bool StrongestIsTeammate(IEnumerable<Card> trickCards, Contract contract)
+        {
+            //Assumes 4 players
+            return trickCards.SkipLast(1).LastOrDefault() == trickCards.StrongestCard(contract);
         }
     }
 }
